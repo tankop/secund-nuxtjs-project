f777ecc5e11e5289f81d5ad7c0bb186e887cb776
tankop <peter.tanko@tancode.hu>
2020-03-14 21:05:49 +0100

    sections and external font

---
+++ pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about-page">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+    </div>
+</template>
+
+<style scoped>
+.about-page{
+    text-align: center;
+    padding: 30px;
+}
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="single-post-page">
+        <section class="post">
+            <h1>Title of the post</h1>
+            <div class="post-details">
+                <div>Last updated on XXX</div>
+                <div>Written by NAME</div>
+            </div>
+        </section>
+        <section class="post-feedback">
+            <p>Let me know what you think about the post, send a mail to <a href="mailto:dev@dev.hu">dev@dev.hu</a></p>
+        </section>
+    </div>
+</template>
