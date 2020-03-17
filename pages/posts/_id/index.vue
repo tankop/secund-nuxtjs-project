41b3ebc40c6389c4654fd01a37972ac9704a4e60
tankop <peter.tanko@tancode.hu>
2020-03-17 15:51:31 +0100

    store

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,17 +1,44 @@
 <template>
-    <div class="single-post-page">
-        <section class="post">
-            <h1>Title of the post</h1>
-            <div class="post-details">
-                <div class="post-detail">Last updated on XXX</div>
-                <div class="post-detail">Written by NAME</div>
-            </div>
-        </section>
-        <section class="post-feedback">
-            <p>Let me know what you think about the post, send a mail to <a href="mailto:dev@dev.hu">dev@dev.hu</a></p>
-        </section>
-    </div>
+  <div class="single-post-page">
+    <section class="post">
+      <h1 class="post-title">{{ loadedPost.title }}</h1>
+      <div class="post-details">
+        <div class="post-detail">Last updated on {{ loadedPost.updatedDate }}</div>
+        <div class="post-detail">Written by {{ loadedPost.author }}</div>
+      </div>
+      <p class="post-content">{{ loadedPost.content }}</p>
+    </section>
+    <section class="post-feedback">
+      <p>
+        Let me know what you think about the post, send a mail to
+        <a href="mailto:feedback@my-awesome-domain.com">feedback@my-awesome-domain.com</a>.
+      </p>
+    </section>
+  </div>
 </template>
+
+<script>
+export default {
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPost: {
+          id: "1",
+          title: "First Post (ID: " + context.route.params.id + ")",
+          previewText: "This is our first post!",
+          author: "Maximilian",
+          updatedDate: new Date(),
+          content:
+            "Some dummy text which is definitely not the preview text though!",
+          thumbnail:
+            "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        }
+      });
+    }, 1000);
+  }
+};
+</script>
+
 
 <style scoped>
 .single-post-page {
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="posts-page">
-      <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
 <script>
-import PostList from '@/components/Posts/PostList'
+import PostList from "@/components/Posts/PostList";
 export default {
-    components: {
-        PostList
+  components: {
+    PostList
+  },
+  computed: {
+    loadedPosts(){
+      return this.$store.getters.loadedPosts
     }
-}
+  }
+};
 </script>
 <style scoped>
 .posts-page {
@@ -18,5 +23,4 @@ export default {
   justify-content: center;
   align-items: center;
 }
-
 </style>
